5cb6b8cd65bb205c4e24a5046b64c9d8d50a5813
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-01-01 18:05:59 -0500

    updated documentation

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Route } from "react-router-dom"
+import { Route } from "react-router"
 
+/**
+ * A react route that uses a different component based on the authentication status.
+ * @param {Object} props
+ * @param {Bool} props.isAuthenticated Is the user authenticated
+ * @param {Function} props.authenticatedComponent The component to render when user is authenticated.
+ * @param {Function} props.anonymousComponent The component to render when the user is.
+ * @param {...Array} props.rest The rest of the arguments.
+ */
 function ToggledRoute({
 	isAuthenticated,
 	authenticatedComponent,
 	anonymousComponent,
-	path,
 	...rest
 }) {
 	if (isAuthenticated) {
-		return <Route path={path} component={authenticatedComponent} {...rest} />
-	} else {
-		return <Route path={path} component={anonymousComponent} {...rest} />
+		return <Route component={authenticatedComponent} {...rest} />
 	}
+	return <Route component={anonymousComponent} {...rest} />
 }
 
 ToggledRoute.propTypes = {
 	isAuthenticated: PropTypes.bool.isRequired,
-	path: PropTypes.string.isRequired,
-	authenticatedComponent: PropTypes.oneOfType([PropTypes.func]).isRequired,
-	anonymousComponent: PropTypes.oneOfType([PropTypes.func]).isRequired
+	authenticatedComponent: PropTypes.oneOfType([
+		PropTypes.func,
+		PropTypes.string
+	]).isRequired,
+	anonymousComponent: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+		.isRequired
 }
 
 export default ToggledRoute
